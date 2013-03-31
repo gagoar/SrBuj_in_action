71c1bf38e0c27672799c3fb762f94230b11cbdd7
gagoar <xeroice@gmail.com>
2013-03-31 20:23:06 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+gem 'haml', '3.1.6'
+gem 'haml-rails'
+gem 'unicorn'
+gem 'binding_of_caller'
+gem 'better_errors'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier',     '>= 1.0.3'
+  gem 'therubyracer'
+  gem 'less'
+  gem 'less-rails-bootstrap'
+  gem 'jquery-rails'
+  gem 'SrBuj', '0.5.1'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SrBujInAction::Application.config.secret_token = 'bcb05a96c3bb9e32a452a49ecc1dc3f72404d513470d49b0ae12ee31120b6b30a8d5b138d88207fe32fbd8e682c65b11b28c98396a817c99519cb74d12844615'
