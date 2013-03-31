6358e3230fa1d56fa7c975c1415146250aa748b6
gagoar <xeroice@gmail.com>
2013-03-31 20:47:16 -0300

    all views and migrations

---
+++ db/migrate/20130331232820_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.integer :list_id
+      t.string  :content
+      t.date    :done_at
+      t.timestamps
+    end
+  end
+end
